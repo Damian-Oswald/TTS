02a4a11358514e8e2168829b612824c34951892e
Damian-Oswald <damian.oswald@protonmail.com>
2023-11-10 19:34:52 +0100

    command line arguments

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This code only serves the purpose to test OpenAIs new text-to-speech (TTS) model.
 
-Based on the text in the file `transcript.txt`, the program will generate an MP3 file for [every available voice](https://platform.openai.com/docs/quickstart?context=python) and save these in the "speech" folder.
+Based on the text in the file `transcript.txt`, the program will generate an MP3 file for [every available voice](https://platform.openai.com/docs/quickstart?context=python) and save these in the `speech` folder.
 
 # How can I run the program?
 
@@ -13,3 +13,11 @@ You can run the script directly from the console as follows.
 ```
 python3 TTS.py
 ```
+
+By default, the `alloy` voice will be used. However, you can set any number of other voices via command line arguments.
+
+```
+python3 TTS.py nova fable
+```
+
+Or, if you'd like to generate all possible voices at once, simply use `python3 TTS.py nova fable`.
--- TTS.py
+++ TTS.py
@@ -1,17 +1,30 @@
 from pathlib import Path
 from openai import OpenAI
+import sys
+
+# read terminal arguments
+arguments = sys.argv[1:len(sys.argv)]
+
+# define which arguments are valid
+valid = ["alloy","echo","fable","onyx","nova","shimmer"]
+
+# check which voices are valid and define a voice list
+voices = [i for i in arguments if i in valid]
+if(len(voices)==0):
+  voices = "alloy"
+if("all" in arguments):
+  voices = ["alloy","echo","fable","onyx","nova","shimmer"]
 
 # open connection to
 client = OpenAI(api_key = "<SECRET-API-KEY>")
+client = OpenAI(api_key = "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")
 
 # read video transcript
 with open("transcript.txt") as f:
     transcript = f.readlines()
 
-print(transcript)
-
 # read the transcript once in every available language
-for voice in ["alloy","echo","fable","onyx","nova","shimmer"]:
+for voice in voices:
   filename = "speech-" + voice + ".mp3"
   speech_file_path = Path(__file__).parent / "speech" / filename
   response = client.audio.speech.create(
