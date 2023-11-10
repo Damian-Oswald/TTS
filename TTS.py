0d20784aeb5f341c6dbfb82edf597df59a25668a
Damian-Oswald <2133695@blv-pw-app-01.hse.begasoft.ch>
2023-11-10 17:35:44 +0100

    added all files

---
+++ TTS.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from openai import OpenAI
+
+# open connection to 
+client = OpenAI(api_key = "<SECRET-API-KEY>")
+
+# read video transcript
+with open("transcript.txt") as f:
+    transcript = f.readlines()
+
+# read the transcript once in every available language
+for voice in ["alloy","echo","fable","onyx","nova","shimmer"]:
+  filename = "speech-" + voice + ".mp3"
+  speech_file_path = Path(__file__).parent / "speech" / filename
+  response = client.audio.speech.create(
+    model = "tts-1",
+    voice = voice,
+    input = "".join(transcript)
+  )
+  response.stream_to_file(speech_file_path)
